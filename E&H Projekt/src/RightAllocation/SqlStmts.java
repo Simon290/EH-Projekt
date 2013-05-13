e080b6b5f9e2ff64d155e7b710e8160c9767bb45
Pascal <Pascal@Pascal-PC>
2013-05-13 11:47:27 +0200

    v 1.03

--- E&H Projekt/src/RightAllocation/SqlStmts.java
+++ E&H Projekt/src/RightAllocation/SqlStmts.java
@@ -3,7 +3,12 @@ package RightAllocation;
 public class SqlStmts {
 	
 	static String generateUserInfoSQLStmt(String userId){
-		String sql = "SELECT * FROM Users WHERE User_ID =" + userId;
+		String sql = "SELECT * FROM Users WHERE User_ID =" + userId + ";";
+		return sql;
+	}
+	
+	static String generateGetTableSQLStmt(String appId){
+		String sql = "SELECT Table_Roles, Table_Ressources FROM Applications WHERE App_ID =" + appId + ";";
 		return sql;
 	}
 
