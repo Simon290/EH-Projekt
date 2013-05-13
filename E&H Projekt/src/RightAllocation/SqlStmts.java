43b2c6e327c7484dcd96baa8cd4cc5524fdb908b
Simon290 <simon@skaletons.de>
2013-05-13 11:33:45 +0200

    v 1.02

--- E&H Projekt/src/RightAllocation/SqlStmts.java
+++ E&H Projekt/src/RightAllocation/SqlStmts.java
@@ -2,6 +2,9 @@ package RightAllocation;
 
 public class SqlStmts {
 	
-	
+	static String generateUserInfoSQLStmt(String userId){
+		String sql = "SELECT * FROM Users WHERE User_ID =" + userId;
+		return sql;
+	}
 
 }
