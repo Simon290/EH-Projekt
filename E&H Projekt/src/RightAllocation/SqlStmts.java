7ab94f96dc98254ba1ce01896ce6da89205b48c1
Pascal <Pascal@Pascal-PC>
2013-05-13 11:57:58 +0200

    v 1.05 Methoden erweitert

--- E&H Projekt/src/RightAllocation/SqlStmts.java
+++ E&H Projekt/src/RightAllocation/SqlStmts.java
@@ -1,14 +1,20 @@
 package RightAllocation;
 
 public class SqlStmts {
-	
-	static String generateUserInfoSQLStmt(String userId){
+
+	static String generateUserInfoSQLStmt(String userId) {
 		String sql = "SELECT * FROM Users WHERE User_ID =" + userId + ";";
 		return sql;
 	}
 	
-	static String generateGetTableSQLStmt(String appId){
-		String sql = "SELECT Table_Roles, Table_Ressources FROM Applications WHERE App_ID =" + appId + ";";
+	static String generateGetRoleIdSQLStmt(String userId, String appId) {
+		String sql = "SELECT Role_ID, Ressource_ID FROM Main WHERE User_ID like '" + userId + "' AND App_ID like '" + appId + ";";
+		return sql;
+	}
+
+	static String generateGetTableSQLStmt(String appId) {
+		String sql = "SELECT Table_Roles, Table_Ressources FROM Applications WHERE App_ID ="
+				+ appId + ";";
 		return sql;
 	}
 
