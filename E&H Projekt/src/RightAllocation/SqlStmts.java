befecd7f70efd8d923ee1038c86437f5c7ef1f8a
AUS55ARI <AUS55ARI@N55AUS01.JG.INTRANET>
2013-05-13 11:56:02 +0200

    v 1.04 Methode AppInfo und RechteAbfrage hinzugefügt

--- E&H Projekt/src/RightAllocation/SqlStmts.java
+++ E&H Projekt/src/RightAllocation/SqlStmts.java
@@ -6,5 +6,16 @@ public class SqlStmts {
 		String sql = "SELECT * FROM Users WHERE User_ID =" + userId;
 		return sql;
 	}
+	
+	static String generateRechteAbfragenStmt(String appId, String userId, String rightsId){
+		String sql = "SELECT * FROM Table_Roles WHERE Role_ID =" + rightsId + ";";
+		return sql;
+	}
 
+	static String generateAppInfoSQLStmt(String appId){
+		String sql = "SELECT * FROM Applications WHERE App_ID =" + appId + ";";
+		return sql;
+	}
+	
+	
 }
